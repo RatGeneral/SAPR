40bfbae9d7b51b8cce3d0acff0b0de102d10cce5
RatGeneral <96421241+RatGeneral@users.noreply.github.com>
2023-01-18 21:27:18 +0300

    Final commit

--- task6/task6.py
+++ task6/task6.py
@@ -1,74 +1,81 @@
 import numpy as np
 
-def rdCSV(csv_file):
-    with open(csv_file) as file:
-        pre_res = []
-        res = []
-        nd_row = []
+CSV_PATH = "./task6_data.csv"
+
+
+def readCSV():
+    with open(CSV_PATH) as file:
+        pre_result = []
+        result = []
+        my_row = []
         csv_string = file.read()
         csv_string = csv_string.split("\n")
         for row in csv_string:
-            pre_res.append(eval(row))
-        for i in range(0, len(pre_res[0])):
-            for j in range(0, len(pre_res[0])):
-                nd_row.append(pre_res[j][i])
-            res.append(nd_row)
-            nd_row = []
-    return res
+            pre_result.append(eval(row))
+        for i in range(0, len(pre_result[0])):
+            for j in range(0, len(pre_result[0])):
+                my_row.append(pre_result[j][i])
+            result.append(my_row)
+            my_row = []
+    return result
 
-def mMyMatrix(matrix):
-    new_row = []
+
+def build_matrix(matrix):
     arr_matrix = []
+    new_row = []
     for k in range(0, len(matrix)):
         for i in range(0, len(matrix[k])):
             for j in range(0, len(matrix[k])):
-                if (matrix[k][i] < matrix[k][j]):
+                if matrix[k][i] < matrix[k][j]:
                     new_row.append(1)
-                elif (matrix[k][i] == matrix[k][j]):
+                elif matrix[k][i] == matrix[k][j]:
                     new_row.append(0.5)
-                elif (matrix[k][i] > matrix[k][j]):
+                elif matrix[k][i] > matrix[k][j]:
                     new_row.append(0)
         arr_matrix.append(new_row)
         new_row = []
     return arr_matrix
 
-def mfMatrix(matrixs):
-    fMatrix = []
+
+def build_matrix_1(matrixs):
+    oneMatrix = []
     for i in range(0, len(matrixs[0])):
-        sum = 0
+        summa = 0
         for k in range(0, len(matrixs)):
-            sum += matrixs[k][i]
-        fMatrix.append(sum/len(matrixs))
-    return fMatrix
+            summa += matrixs[k][i]
+        oneMatrix.append(summa / len(matrixs))
+    return oneMatrix
 
 
 def split(a, n):
     k, m = divmod(len(a), n)
-    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
-def mKMatrix(fMatrix, k = [1/3, 1/3, 1/3]):
-    kMatrix = list(split(fMatrix, len(k)))
+def build_matrix_k(oneMatrix, k=[1 / 3, 1 / 3, 1 / 3]):
+    kMatrix = list(split(oneMatrix, len(k)))
     n = len(kMatrix[0])
-    kP = np.ones(n) / n
-    kN = None
+    k_p = np.ones(n) / n
+
     while True:
-        y = np.matmul(kMatrix, kP)
+        y = np.matmul(kMatrix, k_p)
         lbd = np.matmul(np.ones(n), y)
         kN = (1 / lbd) * y
-        diff = abs(kN - kP)
+        diff = abs(kN - k_p)
         max = diff.max()
         if max <= 0.001:
             break
         else:
-            kP = kN
+            k_p = kN
     return np.around(kN, 3)
 
-def task(csvString):
-    res1 = readCSV(csvString)
-    res2 = mMyMatrix(res1)
-    res3 = mfMatrix(res2)
-    res4 = mKMatrix(res3)
+
+def task():
+    res1 = readCSV()
+    res2 = build_matrix(res1)
+    res3 = build_matrix_1(res2)
+    res4 = build_matrix_k(res3)
     return res4
-    
-print(task("./task6_data.csv"))
+
+
+print(task())
